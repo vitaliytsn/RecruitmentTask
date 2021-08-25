1de40fcd74ef614bf8551bc450662bddb8559e03
Vitalii Tsekhanovskyi <s22186@pjwstk.edu.pl>
2021-08-25 17:32:12 +0100

    added Put/Post validaions

--- RecruitmentTask/DTOS/UpadateEntityDto.cs
+++ RecruitmentTask/DTOS/UpadateEntityDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace RecruitmentTask.DTOS
 {
     public class UpadateEntityDto
     {
+        [Required]
         public string Name { get; set; }
 
+        [Required]
+        [Range(1, 1000)]
         public decimal Price { get; set; }
     }
 }
